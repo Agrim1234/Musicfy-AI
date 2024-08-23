90507c7f9441081aa61f4129f873461c3b4eae7d
Agrim1234 <agrimagarwal123456789@gmail.com>
2024-08-23 16:55:26 +0530

    image optimization made

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -3,6 +3,7 @@
 import React from 'react'
 import { useSession, signIn, signOut } from "next-auth/react"
 import { useRouter } from 'next/navigation'
+import Image from 'next/image'
 
 const Page = () => {
   const { data: session } = useSession()
@@ -20,23 +21,23 @@ const Page = () => {
       <div className='flex flex-col'>
 
         <button type="button" className="text-white bg-[#3b5998] hover:bg-[#3b5998]/90 focus:ring-4 focus:outline-none focus:ring-[#3b5998]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#3b5998]/55 me-2 mb-2 flex gap-2 justify-around w-56">
-          <img src="/facebook.svg" alt="" width={10} height={20} className='invert' />
+          <Image src="/facebook.svg" alt="login method" width={10} height={20} className='invert' />
           <span> Sign in with Facebook </span>
         </button>
         <button type="button" className="text-white bg-[#1da1f2] hover:bg-[#1da1f2]/90 focus:ring-4 focus:outline-none focus:ring-[#1da1f2]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#1da1f2]/55 me-2 mb-2 flex gap-2 justify-around w-56">
-          <img src="/twitter.svg" alt="" width={24} height={24} className='invert' />
+          <Image src="/twitter.svg" alt="login method" width={24} height={24} className='invert' />
           <span> Sign in with Twitter </span>
         </button>
         <button type="button" className="text-white bg-[#24292F] hover:bg-[#24292F]/90 focus:ring-4 focus:outline-none focus:ring-[#24292F]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-500 dark:hover:bg-[#050708]/30 me-2 mb-2 flex gap-2 justify-around w-56" onClick={() => signIn("github")}>
-          <img src="github.svg" alt="" width={24} height={24} className='invert' />
+          <Image src="github.svg" alt="login method" width={24} height={24} className='invert' />
           <span> Sign in with Github </span>
         </button>
         <button type="button" className="text-white bg-[#4285F4] hover:bg-[#4285F4]/90 focus:ring-4 focus:outline-none focus:ring-[#4285F4]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#4285F4]/55 me-2 mb-2 flex gap-2 w-56 justify-around" onClick={() => signIn("google")}>
-          <img src="/google.svg" alt="" width={24} height={24} className='invert' />
+          <Image src="/google.svg" alt="login method" width={24} height={24} className='invert' />
           <span> Sign in with Google </span>
         </button>
         <button type="button" className="text-white bg-[#050708] hover:bg-[#050708]/90 focus:ring-4 focus:outline-none focus:ring-[#050708]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#050708]/50 dark:hover:bg-[#050708]/30 me-2 mb-2 flex justify-around gap-2 w-56">
-          <img src="/apple.svg" alt="" width={24} height={24} className='invert' />
+          <Image src="/apple.svg" alt="login method" width={24} height={24} className='invert' />
           <span> Sign in with Apple </span>
         </button>
       </div>
